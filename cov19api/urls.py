99c5599add61eb4dd71795f70307d601538f552f
Mathieu Voland <mathieu.voland@gmail.com>
2021-04-16 23:00:26 +0200

    Simple API test endpoint

--- cov19api/urls.py
+++ cov19api/urls.py
@@ -1,13 +1,17 @@
+from django.conf.urls import url
 from django.urls import include, path
 from rest_framework import routers
-from . import views
+
+from .views import TestView, RegionViewSet
 
 router = routers.DefaultRouter()
-router.register(r'regions', views.RegionViewSet)
+router.register(r'regions', RegionViewSet)
+router.register(r'test', TestView, 'toto')
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    # path('test', TestView.as_view())
 ]
--- cov19api/views.py
+++ cov19api/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
-
-from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.viewsets import ModelViewSet, ViewSet
 
 from .serializers import *
 from .models import *
 
 
-class RegionViewSet(viewsets.ModelViewSet):
+class RegionViewSet(ModelViewSet):
     queryset = Region.objects.all().order_by('code')
     serializer_class = RegionSerializer
+
+
+class TestView(ViewSet):
+    def list(self, request):
+        return Response({'test': 'test'})
