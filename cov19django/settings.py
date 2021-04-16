367ad6151839efcc06f841bbd728ccad200224be
Mathieu Voland <mathieu.voland@gmail.com>
2021-04-16 11:37:03 +0200

    Add django_heroku

--- cov19django/settings.py
+++ cov19django/settings.py
@@ -10,6 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import django_heroku
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
+
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
