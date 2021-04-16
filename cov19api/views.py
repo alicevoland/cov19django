df7053ffb5d79c004b978ddd36d3fd11aa3ed418
Mathieu Voland <mathieu.voland@gmail.com>
2021-04-16 11:20:42 +0200

    Basic REST API for Region model

--- cov19api/views.py
+++ cov19api/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from rest_framework import viewsets
+
+from .serializers import *
+from .models import *
+
+
+class RegionViewSet(viewsets.ModelViewSet):
+    queryset = Region.objects.all().order_by('code')
+    serializer_class = RegionSerializer
